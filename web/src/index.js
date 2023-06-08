6910a91445a4a5998e96ce842222bb5695fa77e3
Nuria Castaño <codesalnuria@gmail.com>
2023-06-08 13:29:58 +0200

    add strucutre folders

--- web/src/index.js
+++ web/src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
   </HashRouter>
 );
