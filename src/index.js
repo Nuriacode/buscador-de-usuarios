beacead34d63f21bfb4c20d2926d5e21f8b42d0a
Nuria Castaño <codesalnuria@gmail.com>
2023-06-08 15:46:42 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const cors = require('cors');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import { HashRouter } from 'react-router-dom';
+import './components/styles/reset/reset.scss'
 
-const app = express();
-app.use(cors());
-app.use(express.json({limit:"10mb"}));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <HashRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  </HashRouter>
+);
 
-const port = 4000;
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
